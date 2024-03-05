cc30528c6dcf1b791fd0ef1a916eabbc41413422
student-shoaibsoomro <159219257+student-shoaibsoomro@users.noreply.github.com>
2024-03-05 05:01:00 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,2 @@
+"use strict";
+console.log("Albert Ienstein once said, 'A person who never made mistake never tried anything new.'");
---
+++ app.ts
@@ -0,0 +1,1 @@
+console.log("Albert Ienstein once said, 'A person who never made mistake never tried anything new.'")
